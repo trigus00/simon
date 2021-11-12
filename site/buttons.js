5d7602a5f1719f01d8921751f919159fba656094
Gustavo Mendoza <trigus00@gmail.com>
2021-11-11 19:26:16 -0600

    starter files for sime

---
+++ site/buttons.js
@@ -0,0 +1,19 @@
+let greenButton = document.querySelector('.simon-button.green');
+greenButton.addEventListener('click', function() {
+  alert('You clicked the green button');
+});
+
+let redButton = document.querySelector('.simon-button.red');
+redButton.addEventListener('click', function() {
+  alert('You clicked the red button');
+});
+
+let yellowButton = document.querySelector('.simon-button.yellow');
+yellowButton.addEventListener('click', function() {
+  alert('You clicked the yellow button');
+});
+
+let blueButton = document.querySelector('.simon-button.blue');
+blueButton.addEventListener('click', function() {
+  alert('You clicked the blue button');
+});
