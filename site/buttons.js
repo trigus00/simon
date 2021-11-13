6107d523a9f1edcd26520eef1ad83aa1a547bc66
Gustavo Mendoza <trigus00@gmail.com>
2021-11-12 23:44:48 -0600

    updates

--- site/buttons.js
+++ site/buttons.js
@@ -1,39 +1,92 @@
-require('prompt-sync')();
+
 
 let greenButton = document.querySelector('.simon-button.green');
+let count = 0; 
 
-greenButton.addEventListener('click', function () {
-    console.log("I'm green");
+// Green Button On and off 
+greenButton.addEventListener('mousedown', function () {
+  
+  if (pressed) {
+    greenButton.classList.add('light')
+    var audio = new Audio("sound-1.mp3");
+    audio.play();
+    count ++; 
+    console.log("green: " + count)
+  }
+})
+
+greenButton.addEventListener("mouseup", function () {
+  if (pressed) {
+    greenButton.classList.remove('light')
+  }
+
+})
+
+
+// Red botton On and Off 
+let redButton = document.querySelector('.simon-button.red');
+
+redButton.addEventListener('mousedown', function () {
+  
+  if (pressed) {
+    redButton.classList.add('light')
+    // Audio 
+    var audio = new Audio("sound-2.mp3");
+    audio.play();
+    count ++; 
+    console.log("red: " + count)
+   
+  }
+});
+redButton.addEventListener("mouseup", function () {
+  if (pressed) {
+    redButton.classList.remove('light')
+  }
+})
+
+
+
+// Yellow button On and Off 
+  let yellowButton = document.querySelector('.simon-button.yellow');
+
+  yellowButton.addEventListener('mousedown', function () {
+    
+    if(pressed){
+      
+      yellowButton.classList.add('light')
+    // Audio 
+    var audio = new Audio("sound-3.mp3");
+    audio.play();
+    count ++; 
+    console.log("Yellow: " + count)
+  }
+  });
+  yellowButton.addEventListener("mouseup",function(){
+    if(pressed){
+      yellowButton.classList.remove('light')
+    }
   })
 
 
-let redButton = document.querySelector('.simon-button.red');
-redButton.addEventListener('click', function () {
-  //alert('You clicked the red button');
-  // Audio 
-  var audio = new Audio("sound-2.mp33");
-  audio.play();
-  document.getElementById("red")
+  // Blue button On and Off 
 
-  console.log("I's red")
-});
+  let blueButton = document.querySelector('.simon-button.blue');
+  blueButton.addEventListener('mousedown', function () {
+ 
+    if(pressed){
+      blueButton.classList.add('light')
+    // Audio 
+    var audio = new Audio("sound-4.mp3");
+    audio.play();
+    count ++; 
+    console.log("blue "+ count)
+   
+    }
+  });
+  blueButton.addEventListener('mouseup',function(){
+    if(pressed){
+      blueButton.classList.remove('light')
+    }
 
-let yellowButton = document.querySelector('.simon-button.yellow');
-yellowButton.addEventListener('click', function () {
-  //alert('You clicked the yellow button');
-  // Audio 
-  var audio = new Audio("sound-3.mp33");
-  audio.play();
-  document.getElementById("yellow")
-  console.log("I'm yellow ")
-});
 
-let blueButton = document.querySelector('.simon-button.blue');
-blueButton.addEventListener('click', function () {
-  //alert('You clicked the blue button');
-  // Audio 
-  var audio = new Audio("sound-4.mp33");
-  audio.play();
-  document.getElementById("blue")
-  console.log("I'm blue ")
-});
+  })
--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,34 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
+let pressed = true;
+let game_list = []; 
+
+function createList(list) {
+    let colors = ["green", "red", "yellow", "blue"]
+    let random = Math.floor(Math.random() * 4);
+
+    switch (random) {
+        case 0:
+            list.push("green");
+            break;
+        case 1:
+            list.push("red");
+            break;
+        case 2:
+            list.push("blue");
+            break;
+        case 3:
+            list.push("yellow");
+            break;
+    }
+
+    console.log(list)
+
+
+}
+
+
+
+let sample = ["red, blue, blue, green"];
+
+createList(game_list)
